51fccd644c71f9f4d345b483d63466fa4b1eade5
BalashovM <mi-h-a@mail.ru>
2021-03-22 01:24:45 +0700

    Lesson01 Init

---
+++ WeatherAPI/WeatherAPI/Controllers/CrudController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrudController : ControllerBase
+    {
+        private readonly ValuesHolder holder;
+        public CrudController(ValuesHolder holder)
+        {
+            this.holder = holder;
+        }
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string input)
+        {
+            holder.Values.Add(input);
+            return Ok();
+        }
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(holder.Values);
+        }
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringsToUpdate, [FromQuery] string newValue)
+        {
+            for (int i = 0; i < holder.Values.Count; i++)
+            {
+                if (holder.Values[i] == stringsToUpdate)
+                    holder.Values[i] = newValue;
+            }
+            
+            return Ok();
+        }
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringToDelete)
+        {
+            holder.Values = holder.Values.Where(w => w != stringToDelete).ToList();
+            return Ok();
+        }
+    }
+}
